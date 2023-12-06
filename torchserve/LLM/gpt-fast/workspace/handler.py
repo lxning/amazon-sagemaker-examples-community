81b9cd15c34c14a1f95f179798f4e4de38d7ba7c
lxning <lninga@amazon.com>
2023-12-06 11:25:56 -0800

    add AOT

--- torchserve/LLM/gpt-fast/workspace/handler.py
+++ torchserve/LLM/gpt-fast/workspace/handler.py
@@ -1,4 +1,4 @@
- import json
+import json
 import logging
 import os
 import time
@@ -8,10 +8,10 @@ import subprocess
 import shutil
 import sys
 
-sys.path.append('/gpt-fast')
+sys.path.append('/home/model-server/gpt-fast')
 
 import torch
-from generate import _load_model, decode_one_token, encode_tokens, prefill, speculative_decode
+from generate import _load_model, decode_one_token, encode_tokens, prefill, speculative_decode, model_forward
 from model import Transformer
 from sentencepiece import SentencePieceProcessor
 
@@ -36,32 +36,35 @@ class GptHandler(BaseHandler):
         self.prompt_length = 0
         self.speculate_k = 8
         self.draft_model = None
+        self.is_speculative = False
 
     def initialize(self, ctx):
         self.context = ctx
         properties = ctx.system_properties
-        if torch.cuda.is_available() and properties.get("gpu_id") is not None:
-            self.map_location = "cuda"
-            self.device = torch.device(
-                self.map_location + ":" + str(properties.get("gpu_id"))
-            )
+        if torch.cuda.is_available():
+            self.device = 'cuda'
 
-        quantization = ctx.model_yaml_config["handler"]["quantization"]
+        quantization = ctx.model_yaml_config["handler"].get("quantization", "int8")
         model_dir = ctx.system_properties.get("model_dir")
-
+ 
         model_name = ctx.model_yaml_config["handler"]["model_name"]
-        os.environ["MODEL_REPO"] = model_name
-        cmd = ["sh", "scripts/prepare.sh", model_name]
-        run_script = subprocess.Popen(cmd, cwd="/gpt-fast")
-        run_script.wait()
+        checkpoint_root_path = f'{model_dir}/{model_name}/checkpoints'
+        logger.info(f"checkpoint_root_path={checkpoint_root_path}")
+        if not os.path.exists(checkpoint_root_path):
+            os.environ["MODEL_REPO"] = model_name
+            cmd = ["sh", "scripts/prepare.sh", model_name]
+            run_script = subprocess.Popen(cmd, cwd="/home/model-server/gpt-fast")
+            run_script.wait()
 
-        draft_model_name = ctx.model_yaml_config["handler"]["draft_model_name"]
-        os.environ["draft_model_name"] = draft_model_name
-        cmd = ["sh", "scripts/prepare.sh", draft_model_name]
-        run_script = subprocess.Popen(cmd, cwd="/gpt-fast")
-        run_script.wait()
-        shutil.move("/gpt-fast/checkpoints", f'{model_dir}/')
+            draft_model_name = ctx.model_yaml_config["handler"].get("draft_model_name", None)
+            if draft_model_name:
+                os.environ["draft_model_name"] = draft_model_name
+                cmd = ["sh", "scripts/prepare.sh", draft_model_name]
+                run_script = subprocess.Popen(cmd, cwd="/home/model-server/gpt-fast")
+                run_script.wait()
+            shutil.move("/home/model-server/gpt-fast/checkpoints", checkpoint_root_path)
 
+        logger.info("checkpoint_root_path already existed")
         checkpoint_path = Path(f'{model_dir}/{ctx.model_yaml_config["handler"]["converted_ckpt_dir"]}')
         assert checkpoint_path.is_file(), checkpoint_path
 
@@ -74,13 +77,19 @@ class GptHandler(BaseHandler):
         use_tp = False
         if "LOCAL_RANK" in os.environ:
             use_tp = True
-        self.speculate_k = ctx.model_yaml_config["handler"].get("speculate_k", 8)
+            rank = int(os.getenv("LOCAL_RANK", 0))
+            world_size = int(os.getenv("WORLD_SIZE", 0))
+            torch.distributed.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+            torch.cuda.set_device(rank)
+        
+        #self.is_speculative = draft_checkpoint_path is not None
 
         logger.info("Loading model ...")
         t0 = time.time()
         self.model = _load_model(checkpoint_path, self.device, torch.bfloat16, use_tp)
 
-        if draft_checkpoint_path:
+        if self.is_speculative:
+            self.speculate_k = ctx.model_yaml_config["handler"].get("speculate_k", 8)
             self.draft_model = _load_model(draft_checkpoint_path, self.device, torch.bfloat16, use_tp)
 
         torch.cuda.synchronize()
@@ -88,22 +97,29 @@ class GptHandler(BaseHandler):
 
         self.tokenizer = SentencePieceProcessor(model_file=str(tokenizer_path))
 
+        torch.manual_seed(1234)
         if ctx.model_yaml_config["handler"]["compile"]:
+            if self.is_speculative and use_tp:
+                torch._inductor.config.triton.cudagraph_trees = False 
+
+            if self.is_speculative:
+                global model_forward, logits_to_prob
+                model_forward = torch.compile(model_forward, mode="reduce-overhead", fullgraph=True)
             self.decode_one_token = torch.compile(
                 self.decode_one_token, mode="reduce-overhead", fullgraph=True
             )
+
             self.prefill = torch.compile(self.prefill, fullgraph=True, dynamic=True)
 
-        torch.manual_seed(42 * 42)
 
         self.initialized = True
 
     @timed
     def preprocess(self, requests):
+        assert(len(requests) == 1), "GPT fast is currently only supported with batch_size=1"
+
         req_data = requests[0]
-
         input_data = req_data.get("data") or req_data.get("body")
-
         if isinstance(input_data, (bytes, bytearray)):
             input_data = input_data.decode("utf-8")
 
@@ -124,12 +140,19 @@ class GptHandler(BaseHandler):
 
     @timed
     def inference(self, input_data):
+        torch.cuda.synchronize()
         tokenizer = self.tokenizer
         period_id = tokenizer.encode(".")[0]
 
+        done_generating = False
         def call_me(x):
-            nonlocal period_id, tokenizer
-            text = self.tokenizer.decode([period_id] + x.tolist())[1:]
+            nonlocal done_generating, period_id, tokenizer
+            if done_generating:
+                return
+            logger.info(f"period_id={period_id},type={type(period_id)}, x={x.tolist()}, type={type(x)}")
+            text = self.tokenizer.decode([period_id]+x.tolist())[1:]
+            rank=os.getenv("LOCAL_RANK", 0)
+            logger.info(f"text={text}, rank={rank}")
             send_intermediate_predict_response(
                 [text],
                 self.context.request_ids,
@@ -137,10 +160,13 @@ class GptHandler(BaseHandler):
                 200,
                 self.context,
             )
+            if x.item() == tokenizer.eos_id():
+                done_generating = True
 
+        logger.info(f'input_data={input_data["encoded"]}, max_new_tokens={input_data["max_new_tokens"]}') 
         y = self.generate(
-            input_data["encoded"],
-            input_data["max_new_tokens"],
+            prompt=input_data["encoded"],
+            max_new_tokens=input_data["max_new_tokens"],
             speculate_k = self.speculate_k,
             callback=call_me,
             temperature=input_data["temperature"],
@@ -158,7 +184,6 @@ class GptHandler(BaseHandler):
         prompt: torch.Tensor,
         max_new_tokens: int,
         *,
-        draft_model: Transformer,
         speculate_k: Optional[int] = 8,
         callback=lambda x: x,
         **sampling_kwargs,
@@ -167,7 +192,6 @@ class GptHandler(BaseHandler):
         Takes a conditioning sequence (prompt) as input and continues to generate as many tokens as requested.
         """
 
-        is_speculative = draft_model is not None
         # create an empty tensor of the expected final shape and fill in the current tokens
         T = prompt.size(0)
         T_new = T + max_new_tokens
@@ -175,11 +199,11 @@ class GptHandler(BaseHandler):
         max_seq_length = min(T_new, self.model.config.block_size)
 
         device, dtype = prompt.device, prompt.dtype
-        max_seq_length = max_seq_length + speculate_k + 1 if is_speculative else max_seq_length
+        max_seq_length = max_seq_length + speculate_k + 1 if self.is_speculative else max_seq_length
         with torch.device(device):
             self.model.setup_caches(max_batch_size=1, max_seq_length=max_seq_length)
-            if is_speculative and draft_model is not model:
-                draft_model.setup_caches(max_batch_size=1, max_seq_length=max_seq_length)
+            if self.is_speculative and self.draft_model is not self.model:
+                self.draft_model.setup_caches(max_batch_size=1, max_seq_length=max_seq_length)
 
         # create an empty tensor of the expected final shape and fill in the current tokens
         empty = torch.empty(T_new, dtype=dtype, device=device)
@@ -190,31 +214,30 @@ class GptHandler(BaseHandler):
         next_token = self.prefill(
             self.model, prompt.view(1, -1), input_pos, **sampling_kwargs
         )
-
-        period_id = self.tokenizer.encode(".")[0]
-        text = self.tokenizer.decode([period_id] + next_token.tolist())[1:]
-        send_intermediate_predict_response(
-            [text],
-            self.context.request_ids,
-            "Intermediate Prediction success",
-            200,
-            self.context,
-        )
-
-        if is_speculative:
-            prefill(draft_model, prompt.view(1, -1), input_pos, **sampling_kwargs)
+        if self.is_speculative:
+            self.prefill(self.draft_model, prompt.view(1, -1), input_pos, **sampling_kwargs)
         seq[T] = next_token
 
         input_pos = torch.tensor([T], device=device, dtype=torch.int)
         accept_counts = [0] * (speculate_k + 1)
 
-        if is_speculative:
+        #period_id = self.tokenizer.encode(".")[0]
+        #text = self.tokenizer.decode([period_id] + next_token.tolist())[1:]
+        #send_intermediate_predict_response(
+        #    [text],
+        #    self.context.request_ids,
+        #    "Intermediate Prediction success",
+        #    200,
+        #    self.context,
+        #)
+
+        if self.is_speculative:
             input_pos = input_pos.item()  # for speculative decoding easier to keep on host
             while input_pos < T_new - 1:
                 cur_token = next_token.view(())
 
                 next_tokens = speculative_decode(
-                    model, draft_model, cur_token, input_pos, speculate_k, **sampling_kwargs
+                    self.model, self.draft_model, cur_token, input_pos, speculate_k, **sampling_kwargs
                 )
 
                 accept_counts[len(next_tokens) - 1] += 1
